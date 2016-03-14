7ebc3c867bcd4b26a170d316332884659dc54241
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-03-14 20:49:01 +0900

    RedPen 1.5で変更された部分の翻訳を行なった

--- source/plainsentencelengthvalidator.java
+++ source/plainsentencelengthvalidator.java
@@ -3,23 +3,18 @@ package cc.redpen.validator.sentence;
 /**
  * Validate input sentences contain more characters more than specified.
  */
- final public class PlainSentenceLengthValidator extends Validator {
-     /**
-      * Default maximum length of sentences.
-  */
-  public static final int DEFAULT_MAX_LENGTH = 30;
-      private int maxLength = DEFAULT_MAX_LENGTH;
+public class PlainSentenceLengthValidator extends Validator {
 
-      @Override
+  /**
+   * Default constructor initializes properties with their default values.
+   */
+  public PlainSentenceLengthValidator() {
+    super("max_len", 30); // Default maximum length of sentences.
+
+  @Override
   public void validate(Sentence sentence) {
-       if (sentence.getContent().length() > maxLength) {
-           addValidationError(sentence, sentence.getContent().length(), maxLength);
-        }
+    if (sentence.getContent().length() > getInt("max_len")) {
+      addValidationError(sentence, sentence.getContent().length(), maxLength);
+    }
   }
-
-      @Override
-      protected void init() throws RedPenException {
-            this.maxLength = getConfigAttributeAsInt("max_len", DEFAULT_MAX_LENGTH);
-      }
- }
-
+}
